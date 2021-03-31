5d526674aa766891f9a7b3c1a579c22fc6f42da7
jeansberg <jeansberg@gmail.com>
2021-03-31 22:20:06 +0200

    Make LogConsole wider

--- Rogue/Consoles/LogConsole.cs
+++ Rogue/Consoles/LogConsole.cs
@@ -5,7 +5,7 @@ using SadConsole;
 namespace Rogue.Consoles {
     public class LogConsole : SadConsole.Console {
         private readonly Stack<string> messages;
-        public LogConsole() : base(20, 40) {
+        public LogConsole() : base(40, 40) {
             Position = new SadRogue.Primitives.Point(90 + 2, 1);
             messages = new Stack<string>();
         }
